55d7fc88a6d40784e7c087ec0f29d6f5db708165
Esdras <esdraspiano@gmail.com>
2019-12-20 16:09:07 -0300

    Atualização das páginas de publicações e idealizadores.

--- resources/views/projeto/idealizadores.blade.php
+++ resources/views/projeto/idealizadores.blade.php
@@ -4,12 +4,14 @@
 
 	<h1>Idealizadores</h1>
 
-	<p>Este projeto foi fruto de um trabalho de conclusão de curso do <a href="https://computacao.jatai.ufg.br/">Bacharelado em Ciência da Computação</a> da Universidade Federal de Goías, Regional Jataí, no Instituto de Ciências Exatas e Tecnológicas (ICET). Este trabalho foi elaborado pelo discente Frederico Assis Carvalho, sob a orientação do Prof. Esdras Lins Bispo Jr.</p>
+	<p>Este projeto foi fruto dois trabalhos de conclusão de curso do <a href="https://computacao.jatai.ufg.br/">Bacharelado em Ciência da Computação</a> da Universidade Federal de Jataí, no Instituto de Ciências Exatas e Tecnológicas (ICET). O primeiro TCC foi elaborado pelo discente Frederico Assis Carvalho, e o segundo TCC foi elaborado pelo discente Mateus de Oliveira Santos. Ambos os trabalhos foram desenvolvidos sob a orientação do Prof. Esdras Lins Bispo Jr.</p>
 
 	<h2>Contato</h2>
 
-	<p><b>Frederico Assis Carvalho:</b> fredericoassiscarvalho@gmail.com</p>
+    <p><b>Frederico Assis Carvalho:</b> fredericoassiscarvalho@gmail.com</p>
+
+    <p><b>Mateus Oliveira Santos:</b> mateus1996oliveira@hotmail.com</p>
 
 	<p><b>Esdras Lins Bispo Junior:</b> bispojr@ufg.br / esdraspiano@gmail.com</p>
-	
+
 @endsection
--- resources/views/projeto/trabalhos-publicados.blade.php
+++ resources/views/projeto/trabalhos-publicados.blade.php
@@ -3,10 +3,28 @@
 @section('texto-projeto')
 
 	<h1>Trabalhos Publicados</h1>
-    
-	<div class="panel panel-default" style="margin-top: 2.5em;">
-        
-        <div class="panel-heading">    
+
+    <div class="panel panel-default" style="margin-top: 2.5em;">
+
+        <div class="panel-heading">
+            <h4 class="panel-title">
+                GraphViewer: um Sistema de Visualização de Algoritmos no Ensino de Provas por Indução em Teoria dos Grafos
+            </h4>
+        </div>
+
+        <div class="panel-body">
+            Mateus O. Santos, Gecirlei F. Silva,  Esdras L. Bispo Jr.<br>
+            VII Encontro Goiano de Educação Matemática (EnGEM), 2019. <br>
+            <a href="https://www.researchgate.net/publication/333024504_GraphViewer_um_Sistema_de_Visualizacao_de_Algoritmos_no_Ensino_de_Provas_por_Inducao_em_Teoria_dos_Grafos" target="_blank">
+                <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Artigo disponível no ReasearchGate
+            </a>
+        </div>
+
+    </div>
+
+    <div class="panel panel-default" style="margin-top: 2.5em;">
+
+        <div class="panel-heading">
             <h4 class="panel-title">
                 Ensino de Provas por Indução em Grafos utilizando uma Ferramenta Visual de Algoritmos
             </h4>
@@ -14,17 +32,17 @@
 
         <div class="panel-body">
             Frederico A. Carvalho, Flávio F. Borges, Gecirlei F. Silva,  Thiago O. Borges, Esdras L. Bispo Jr.<br>
-            XXII Conferência Internacional sobre Informática na Educação (TISE), 2017. <br>	
+            XXII Conferência Internacional sobre Informática na Educação (TISE), 2017. <br>
             <a href="https://www.researchgate.net/publication/321391290_Ensino_de_Provas_por_Inducao_em_Grafos_utilizando_uma_Ferramenta_Visual_de_Algoritmos" target="_blank">
                 <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Artigo disponível no ReasearchGate
             </a>
         </div>
-        
+
     </div>
 
     <div class="panel panel-default">
-        
-        <div class="panel-heading">    
+
+        <div class="panel-heading">
             <h4 class="panel-title">
                 Ensino de Demonstração por Indução em Teoria dos Grafos utilizando um Ambiente Visual de Algoritmos
             </h4>
@@ -32,7 +50,7 @@
 
         <div class="panel-body">
             Frederico A. Carvalho, Esdras L. Bispo Jr.<br>
-            III Encontro de Licenciaturas do Sudoeste Goiano (ELICPIBID), 2016. <br>	
+            III Encontro de Licenciaturas do Sudoeste Goiano (ELICPIBID), 2016. <br>
             <a href="https://www.researchgate.net/publication/321609171_Ensino_de_Demonstracao_por_Inducao_em_Teoria_dos_Grafos_utilizando_um_Ambiente_Visual_de_Algoritmos" target="_blank">
                 <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Artigo disponível no ReasearchGate
             </a>
@@ -40,6 +58,6 @@
 
     </div>
 
-	
-	
+
+
 @endsection
